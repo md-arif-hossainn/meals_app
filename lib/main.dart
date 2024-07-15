27646dba2acfdf31701ec8c1499232e83e5fa180
hossain_arif <arif134221@gmail.com>
2024-07-16 01:03:54 +0600

    add model class and more screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screen/catagories.dart';
 
+
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
