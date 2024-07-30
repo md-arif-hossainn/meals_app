96e40061b9e51cbbc47f91e4263ec9db0d7b171f
hossain_arif <arif134221@gmail.com>
2024-07-30 22:34:52 +0600

    Fully setup state management

--- lib/screen/tabs.dart
+++ lib/screen/tabs.dart
@@ -1,99 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals_app/provider/favorites_provider.dart';
-import 'package:meals_app/provider/meals_provider.dart';
-import 'package:meals_app/screen/catagories.dart';
-import 'package:meals_app/screen/filters.dart';
-import 'package:meals_app/screen/meals.dart';
-import 'package:meals_app/widgets/main_drawer.dart';
 
-import '../models/meal.dart';
+import '../provider/favorites_provider.dart';
+import '../provider/filters_provider.dart';
+import '../widgets/main_drawer.dart';
+import 'catagories.dart';
+import 'filters.dart';
+import 'meals.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
   Filter.lactoseFree: false,
   Filter.vegetarian: false,
-  Filter.vegan: false,
+  Filter.vegan: false
 };
 
-class TabsScreen extends ConsumerStatefulWidget{
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
-
   @override
-  ConsumerState<TabsScreen> createState(){
+  ConsumerState<TabsScreen> createState() {
     return _TabsScreenState();
   }
 }
 
 class _TabsScreenState extends ConsumerState<TabsScreen> {
-
   int _selectedPageIndex = 0;
-  Map<Filter, bool> _selectedFilters = kInitialFilters;
 
-  void _selectPage(int index){
+  void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
     });
   }
 
-  void _setScreen(String identifier) async{
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
-    if(identifier == 'filters'){
-      final result = await Navigator.of(context).push<Map<Filter,bool>>(
-          MaterialPageRoute(builder: (ctx) =>  FiltersScreen(currentFilters: _selectedFilters,)
-          )
+    if (identifier == 'filters') {
+      await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
       );
-      setState(() {
-        _selectedFilters = result ?? kInitialFilters;
-      });
-      print("------------------------$result");
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final availableMeals = meals.where((meal) {
-      if(_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree){
-        return false;
-      }
-      if(_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree){
-        return false;
-      }
-      if(_selectedFilters[Filter.vegan]! && !meal.isVegan){
-        return false;
-      }
-      if(_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian){
-        return false;
-      }
-      return true;
-    }).toList();
-    Widget activePage =  CategoriesScreen(availableMeals: availableMeals,);
+    final availableMeals = ref.watch(filteredMealsProvider);
+
+    Widget activePage = CategoriesScreen(
+      availableMeals: availableMeals,
+    );
     var activePageTitle = 'Categories';
 
-    if(_selectedPageIndex == 1) {
+    if (_selectedPageIndex == 1) {
       final favoriteMeals = ref.watch(favoriteMealsProvider);
-      activePage =  MealsScreen( meals: favoriteMeals,);
+      activePage = MealsScreen(
+        meals: favoriteMeals,
+      );
       activePageTitle = 'Your Favorites';
     }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: MainDrawer(onSelectScreen: _setScreen,),
-      body: activePage ,
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
+      body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
-          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
         ],
       ),
     );
   }
-
 }
